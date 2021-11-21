b1e8abeeb99a7d08cff29e4eb4c02e7d0de28e47
Hodeem <miller.hodeem@gmail.com>
2021-11-21 15:22:12 -0500

    And now his watch has ended. Not bad for a first try ;)

--- Client.java
+++ Client.java
@@ -1,70 +1,92 @@
+
 // A Java program for a Client
 import java.net.*;
 import java.io.*;
+import javax.swing.*;
 
-public class Client
-{
+public class Client {
 	// initialize socket and input output streams
-	private Socket socket		 = null;
-	private DataInputStream input = null;
-	private DataOutputStream out	 = null;
+	private Socket socket = null;
+	private DataInputStream gameReader;
+	private DataOutputStream gameWriter;
+	PrintWriter loginWriter;
+	BufferedReader loginReader;
 
 	// constructor to put ip address and port
-	public Client(String address, int port)
-	{
+	public Client(String address, int port) {
+
+		Boolean gameInSession = false;
 		// establish a connection
-		try
-		{
+		try {
 			socket = new Socket(address, port);
 			System.out.println("Connected");
 
-			// takes input from terminal
-			input = new DataInputStream(System.in);
+			loginWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-			// sends output to the socket
-			out = new DataOutputStream(socket.getOutputStream());
-		}
-		catch(UnknownHostException u)
-		{
+			// loginReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String username = JOptionPane.showInputDialog(null, "Enter Name");
+
+			loginWriter.println(username);
+
+			Box passwordBox = Box.createHorizontalBox();
+			JLabel label = new JLabel("Password: ");
+			passwordBox.add(label);
+
+			JPasswordField passwordField = new JPasswordField(24);
+			passwordBox.add(passwordField);
+
+			int passwordPane = JOptionPane.showConfirmDialog(null, passwordBox, "Enter your password",
+					JOptionPane.OK_CANCEL_OPTION);
+
+			if (passwordPane == JOptionPane.OK_OPTION) {
+				String passText = new String(passwordField.getPassword());
+				loginWriter.println(passText);
+			}
+
+			loginWriter.flush();
+
+		} catch (UnknownHostException u) {
 			System.out.println(u);
-		}
-		catch(IOException i)
-		{
-			System.out.println(i);
+		} catch (IOException i) {
+			System.out.println("In the first block");
+			i.printStackTrace();
 		}
 
-		// string to read message from input
-		String line = "";
+		try {
 
-		// keep reading until "Over" is input
-		while (!line.equals("Over"))
-		{
-			try
-			{
-				line = input.readLine();
-				out.writeUTF(line);
+			gameReader = new DataInputStream(socket.getInputStream());
+
+			Boolean check = gameReader.readBoolean();
+			gameInSession = check;
+			gameReader.close();
+
+			if (gameInSession) {
+				int choice = chooseOption();
+	
+				gameWriter = new DataOutputStream(socket.getOutputStream());
+				gameWriter.writeInt(choice);
+				gameWriter.flush();
 			}
-			catch(IOException i)
-			{
-				System.out.println(i);
-			}
+
+		} catch (IOException i) {
+			System.out.println("Another one " + i);
 		}
 
-		// close the connection
-		try
-		{
-			input.close();
-			out.close();
-			socket.close();
-		}
-		catch(IOException i)
-		{
-			System.out.println(i);
-		}
+
+
 	}
 
-	public static void main(String args[])
-	{
-		Client client = new Client("127.0.0.1", 5000);
+	public int chooseOption() {
+		Object[] options = { "1", "2", "3" };
+		int choice = JOptionPane.showOptionDialog(null, "Choose a number", "Find the Queen",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+
+		System.out.println("The option chosen is: " + choice);
+		return choice;
+	}
+
+	public static void main(String args[]) {
+		Client client = new Client("127.0.0.1", 7621);
 	}
 }
--- Server.java
+++ Server.java
@@ -1,62 +1,159 @@
+
 // A Java program for a Server
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class Server
-{
-	//initialize socket and input stream
-	private Socket		 socket = null;
+public class Server {
+	private Socket client = null;
 	private ServerSocket server = null;
-	private DataInputStream in	 = null;
 
-	// constructor with port
-	public Server(int port)
-	{
-		// starts server and waits for a connection
-		try
-		{
+	public Server(int port) {
+
+		HashMap<String, String> users = new HashMap<String, String>();
+		users.put("danny", "dre@margh_shelled");
+		users.put("matty", "win&win99");
+
+		ArrayList<ClientObject> clients = new ArrayList<>();
+
+		Boolean gameInSession = false;
+
+		try {
 			server = new ServerSocket(port);
-			System.out.println("Server started");
-
 			System.out.println("Waiting for a client ...");
 
-			socket = server.accept();
-			System.out.println("Client accepted");
+			while (!gameInSession) {
 
-			// takes input from the client socket
-			in = new DataInputStream(
-				new BufferedInputStream(socket.getInputStream()));
+				if (clients.size() == 2) {
+					gameInSession = true;
+					for (int i = 0; i < 5; i++) {
 
-			String line = "";
+						this.setRandomSpotter(clients);
+						clients.get(0).startGame();
+						clients.get(1).startGame();
 
-			// reads message from client until "Over" is sent
-			while (!line.equals("Over"))
-			{
-				try
-				{
-					line = in.readUTF();
-					System.out.println(line);
+						int choiceOne = clients.get(0).getChoice();
+						int choiceTwo = clients.get(1).getChoice();
 
+					}
+
+				} else {
+
+					client = server.accept();
+					System.out.println("Connection established");
+
+					BufferedReader read = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+					String username = read.readLine();
+					String password = read.readLine();
+
+					if (users.containsKey(username)) {
+						if (password.equals(users.get(username))) {
+							System.out.println("Login Successful");
+
+							ClientObject newClient = this.createNewClientObject(client, username);
+							newClient.start();
+							
+							clients.add(newClient);
+
+						}
+
+					} else {
+						System.out.println("Login Failed");
+					}
 				}
-				catch(IOException i)
-				{
-					System.out.println(i);
-				}
+
 			}
-			System.out.println("Closing connection");
 
-			// close connection
-			socket.close();
-			in.close();
-		}
-		catch(IOException i)
-		{
+		} catch (IOException i) {
 			System.out.println(i);
 		}
 	}
 
-	public static void main(String args[])
-	{
-		Server server = new Server(5000);
+	public ClientObject createNewClientObject(Socket client, String username) {
+		ClientObject newThread = new ClientObject(client, null, null, username);
+
+		try {
+			DataInputStream inputStream = new DataInputStream(client.getInputStream());
+			DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
+
+			newThread = new ClientObject(client, inputStream, outputStream, username);
+		} catch (IOException i) {
+			System.out.println(i);
+		}
+
+		return newThread;
 	}
+
+	public void setRandomSpotter(ArrayList<ClientObject> clients) {
+		double randomDouble = Math.random();
+		int randomIndex = (int) Math.round(randomDouble);
+		clients.get(randomIndex).makePlayerTheSpotter();
+	}
+
+	public static void main(String args[]) {
+		Server server = new Server(7621);
+	}
+}
+
+class ClientObject extends Thread {
+
+	Socket socket;
+	DataInputStream input;
+	DataOutputStream output;
+	String username;
+	int count = 0;
+	boolean spotter = false;
+	boolean winner = false;
+
+	public ClientObject(Socket socket, DataInputStream input, DataOutputStream output, String username) {
+		this.socket = socket;
+		this.input = input;
+		this.output = output;
+		this.username = username;
+	}
+
+	public void start() {
+		System.out.println(this.username + " is connected");
+	}
+
+	public void incrementCount() {
+		this.count++;
+	}
+
+	public int getCount() {
+		return this.count;
+	}
+
+	public void makePlayerTheSpotter() {
+		this.spotter = true;
+	}
+
+	public void makePlayerTheDealer() {
+		this.spotter = false;
+	}
+
+	public void setWinner() {
+		this.winner = true;
+	}
+
+	public void startGame() {
+		try {	 
+			this.output.writeBoolean(true);
+		} catch (IOException i) {
+			System.out.println(i);
+		}
+	}
+
+	public int getChoice(){
+		int choice = 0;
+		try {
+			choice = this.input.readInt();
+		} catch (IOException i) {
+			System.out.println(i);
+		}
+		return choice;
+
+	}
+
 }
